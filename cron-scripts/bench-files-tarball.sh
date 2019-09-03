51e1fb9ecb5ea810f4b8e56fff7d2b553c4b7339
hb <hb@aroma-project.org>
2019-09-03 11:59:52 -0700

    Benchmark also the deprecated NetApp drive (as a reference)

--- cron-scripts/bench-files-tarball.sh
+++ cron-scripts/bench-files-tarball.sh
@@ -3,6 +3,6 @@
 # shellcheck disable=SC2034
 export BENCH_LOGPATH=$HOME/wynton-bench-logs/$HOSTNAME
 
-for dir in /tmp /scratch /wynton/scratch $HOME /wynton/group/cbi; do
+for dir in /tmp /scratch /wynton/scratch $HOME /wynton/group/cbi /netapp/home/$USER; do
     TEST_DRIVE="$dir" "$BENCH_HOME/bench-scripts/bench-files-tarball.sh"
 done
