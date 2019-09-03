e35414e339ffc1f0a98fa478d179bb28611c780e
Henrik Bengtsson <hb@aroma-project.org>
2019-09-03 08:17:12 -0700

    bench() outputs more stats

--- utils/bench.sh
+++ utils/bench.sh
@@ -22,14 +22,6 @@
 ## %I ru_inblock    Number of file system inputs by the process.
 ## %O ru_oublock    Number of file system outputs by the process.
 ## 
-## %x exit_status   Exit status of the command.
-## %C command       Name and command line arguments of the command being timed.
-##
-##
-##
-## %F ru_majflt     Number of major, or I/O-requiring, page faults that occurred while the process was running.  These are faults where the page has actually migrated out of primary memory.
-## %R ru_minflt     Number of minor, or recoverable, page faults.  These are pages that are not valid (so they fault) but which have not yet been claimed by other virtual pages.  Thus the data in the page is still valid but the system tables must be updated.
-## 
 ## %r ru_msgrcv     Number of socket messages received by the process.
 ## %s ru_msgsnd     Number of socket messages sent by the process.
 ## 
@@ -37,14 +29,22 @@
 ## 
 ## %M ru_maxrss     Maximum resident set size of the process during its lifetime, in Kilobytes.
 ## %t ru_avgrss     Average resident set size of the process, in Kilobytes.
-## 
-## %K               Average total (data+stack+text) memory use of the process, in Kilobytes.
 ## %W ru_nswap      Number of times the process was swapped out of main memory.
+##
+## %F ru_majflt     Number of major, or I/O-requiring, page faults that occurred while the process was running.  These are faults where the page has actually migrated out of primary memory.
+## %R ru_minflt     Number of minor, or recoverable, page faults.  These are pages that are not valid (so they fault) but which have not
+##
+## %x exit_status   Exit status of the command.
+## %C command       Name and command line arguments of the command being timed.
+##
+##
+## %K ru_?????      Average total (data+stack+text) memory use of the process, in Kilobytes.
 ## 
-## %X               Average amount of shared text in the process, in Kilobytes.
-## %Z               System's page size, in bytes.  This is a per-system constant, but varies between systems.
-## %D               Average size of the process's unshared data area, in Kilobytes.
-## %p               Average unshared stack size of the process, in Kilobytes.
+## %X ru_??rss      Average amount of shared text in the process, in Kilobytes.
+## %D ru_?drss      Average size of the process's unshared data area, in Kilobytes.
+## %p ru_?srss      Average unshared stack size of the process, in Kilobytes.
+##
+## %Z ru_?????      System's page size, in bytes.  This is a per-system constant, but varies between systems.
 ##############################################################################
 
 ## Constants
@@ -52,8 +52,8 @@ RAMTMPDIR=${RAMTMPDIR:-/tmp}
 BENCH_TIME=$(type -P time)
 BENCH_TIME_OPTS="--quiet"
 BENCH_TIME_OPTS=
-BENCH_FORMAT="%e\t%S\t%U\t%P\t%w\t%c\t%I\t%O\t%x\t%C"
-BENCH_FORMAT_HEADER="ru_wallclock\tru_stime\tru_utime\tcpu_pct\tru_nvcsw\tru_invcsw\t\tru_inblock\tru_outblock\texit_status\tcommand"
+BENCH_FORMAT="%e\t%S\t%U\t%P\t%w\t%c\t%I\t%O\t%r\t%s\t%k\t%M\t%t\t%W\t%F\t%R\t%x\t%C"
+BENCH_FORMAT_HEADER="ru_wallclock\tru_stime\tru_utime\tcpu_pct\tru_nvcsw\tru_invcsw\t\tru_inblock\tru_outblock\tru_msgrcv\tru_msgsnd\tru_nsignals\tru_maxrss\tru_avgrss\tru_nswap\tru_majflt\tru_minflt\texit_status\tcommand"
 
 bench_header() {
     # shellcheck disable=SC2059
