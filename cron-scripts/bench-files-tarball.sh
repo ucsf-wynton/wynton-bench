e75b403c356ad534706ca11298e59a8fb144964f
hb <hb@aroma-project.org>
2019-09-05 17:40:26 -0700

    Make it possible to override the default set of test drives (env var 'TEST_DRIVES')

--- cron-scripts/bench-files-tarball.sh
+++ cron-scripts/bench-files-tarball.sh
@@ -3,6 +3,9 @@
 # shellcheck disable=SC2034
 export BENCH_LOGPATH=$HOME/wynton-bench-logs/$HOSTNAME
 
-for dir in /tmp /scratch /wynton/scratch $HOME /wynton/group/cbi /netapp/home/$USER; do
+# Default drives to be tested
+TEST_DRIVES=${TEST_DRIVES:-/tmp /scratch /wynton/scratch $HOME /wynton/group/cbi /netapp/home/$USER}
+
+for dir in ${TEST_DRIVES}; do
     TEST_DRIVE="$dir" "$BENCH_HOME/bench-scripts/bench-files-tarball.sh"
 done
