7e778b0cebf28ba3add9487000cdf30843b0534b
Henrik Bengtsson <hb@aroma-project.org>
2021-02-03 17:21:47 -0800

    Add a package help page

---
+++ wyntonbench/R/999.package.R
@@ -0,0 +1,12 @@
+#' Package wyntonbench
+#'
+#' Tools for benchmarking the Wynton HPC environment.
+#' Some useful functions:
+#'
+#' * [extract_bench_log()]
+#'
+#' @keywords package
+#' @name wyntonbench-package
+#' @aliases wyntonbench
+#' @docType package
+NULL
